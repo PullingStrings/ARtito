045e65fa3532fdadc243d5214adb1a3d4c93aa5c
Tito Zwane <tito.zwane@cheil.com>
2021-09-30 16:54:13 +0100

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS54rD4AZt8r3sKiI0qrBvN933l4rEcoQ",
+  authDomain: "artito-4dd9c.firebaseapp.com",
+  projectId: "artito-4dd9c",
+  storageBucket: "artito-4dd9c.appspot.com",
+  messagingSenderId: "660477443134",
+  appId: "1:660477443134:web:bcb1e1197291572719656f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
